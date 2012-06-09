04083f70dd264f8f43eec25f33d61d16d381b096
Dave Lowe <mahalowe@gmail.com>
2012-06-09 15:12:48 -0500

    Setup info

--- setup.py
+++ setup.py
@@ -24,37 +24,3 @@ setup(
         "Framework :: Django",
     ]
 )
-
-
-
-PACKAGE = "checkout"
-NAME = "django-checkout"
-DESCRIPTION = "a pluggable app for handling orders and transactions"
-AUTHOR = "Pullswitch"
-AUTHOR_EMAIL = "dave@hellopullswitch.com"
-URL = "http://github.com/pullswitch/django-checkout"
-VERSION = __import__(PACKAGE).__version__
-
-
-setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=read("README.mkd"),
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    license="BSD",
-    url=URL,
-    packages=find_packages(exclude=["tests.*", "tests"]),
-    package_data=find_package_data(PACKAGE, only_in_packages=False),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Framework :: Django",
-    ],
-    zip_safe=False
-)
